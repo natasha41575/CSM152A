9eea6b43d9fd5eef30afb47e202bdc594c823961
152 <nat41575@gmail.com>
2020-01-06 13:14:05 -0800

    having a blast learning FPGAs

--- lab0.v
+++ lab0.v
@@ -1,4 +1,24 @@
-module 4_bit_counter ();
+module 4_bit_counter (rst);
+
+input rst;
 
 // Declaring registers for 4 D Flip-Flops
-reg reg_1;
+reg a0;
+reg a1;
+reg a2;
+reg a3;
+
+always @ (posedge clk)
+    if(rst)
+        a0 <= 1'b0;
+        a1 <= 1'b0;
+        a2 <= 1'b0;
+        a3 <= 1'b0;
+    else
+        a0 <= ~a0;
+        a1 <= a0 ^ a1;
+        a2 <= (a0 & a1) ^ a2;
+        a3 <= ((a0 & a1) & a2) ^ a3;
+    
+
+endmodule
